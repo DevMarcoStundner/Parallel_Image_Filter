386ba3c9c5ce686f347ba1807802c8a6acf558fe
Marco Stundner <marco.ms553@gmail.com>
2022-04-20 14:50:21 +0000

    optarg added

--- src/main.c
+++ src/main.c
@@ -1,13 +1,69 @@
+/*
+* Author: Stundner Marco
+* Filename: main.c
+* Date 20.04.22
+*/
+
+
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <limits.h>
+
+void print_help (void);
 
 int main(int argc, char **argv) 
 {
-    printf("Hello World");
+    int opt;
+	const char *iarg = NULL;
+	const char *oarg = NULL;
+    const char *karg = NULL;
+	const char *parg = NULL;
 
-    return 0;
+	if (argc == 1) {
+		fprintf (stderr, "This program needs arguments....\n\n");
+		print_help();
+	}
+
+	while ( (opt = getopt (argc, argv, "p:k:i:o:h")) != -1) {
+		switch (opt) {
+		case 'p':
+			parg = optarg;
+			break;
+		case 'k':
+			karg = optarg;
+			break;
+        case 'i':
+			iarg = optarg;
+			break;
+        case 'o':
+			oarg = optarg;
+			break;
+		case 'h': 
+			print_help();
+			break;
+		case ':':
+			fprintf (stderr, "Option requires an argument.\n");
+			print_help ();
+			break;
+		case '?':
+			fprintf (stderr, "Unknown option character %c.\n", optopt);
+			print_help ();
+		}
+	}
+	for (; optind < argc; optind++)
+		printf ("Positional argument %d: %s\n", optind, argv[optind]);
+
+	
+	return 0;
+}
+
+void print_help (void)
+{
+	printf ("\nPI_Filter: prog.exe [-p NUMBER_OF_PROCESSES] -k KERNEL -i INPUT_IMAGE -o OUTPUT_IMAGE [-h]\n");
+	exit (EXIT_FAILURE);
 }
